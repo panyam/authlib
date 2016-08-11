d3be1e5c41e5981715bbe3c1156ee54cdd4aa655
Shobhit Gupta <shobhitg@yahoo-inc.com>
2016-08-10 21:39:02 -0700

    Minor updates to user model

--- userservice/models.py
+++ userservice/models.py
@@ -1,13 +1,27 @@
 
 from google.appengine.ext import ndb
+from google.appengine.ext.ndb import msgprop
 from servicelib import models as slmodels
+
 
 class User(slmodels.ModelBase):
     fullname = ndb.StringProperty()
+    password_hash = ndb.StringProperty(default="")
     phone = ndb.StringProperty(default="")
     email = ndb.StringProperty(default="")
     is_active = ndb.BooleanProperty(default=True)
     last_update_at = ndb.DateTimeProperty(auto_now_add=True)
+    authy_id = ndb.IntegerProperty()
+    authy_status = msgprop.EnumProperty(AUTHY_STATUSES, required=True, indexed=True)
+
+    AUTHY_STATUSES = (
+        'unverified',
+        'onetouch',
+        'sms',
+        'token',
+        'approved',
+        'denied'
+    )
 
     def to_json(self):
         return {
@@ -16,5 +30,6 @@ class User(slmodels.ModelBase):
             'email': self.email,
             'is_active': self.is_active,
             'phone': self.phone,
-            'last_updated': self.last_update_at.ctime()
+            'last_updated': self.last_update_at.ctime(),
+            'authy_status': self.authy_status
         }
